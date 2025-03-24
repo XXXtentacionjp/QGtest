4004eb54a6dd0c5c4e9fe0eb95b1c8d204f660d0
XXXtentacionjp <2092227634@qq.com>
2025-03-24 09:20:55 +0800

    Add files via upload

--- Github/QGtest/3124004644周成羲 计算机学院/大组/week1/week2.cpp
+++ Github/QGtest/3124004644周成羲 计算机学院/大组/week1/week2.cpp
@@ -5,35 +5,66 @@
 
 #define MAX_SIZE 100
 
-// 定义栈结构
+// 定义数值栈结构
+typedef struct {
+    double data[MAX_SIZE];
+    int top;
+} NumStack;
+
+// 定义运算符栈结构
 typedef struct {
     char data[MAX_SIZE];
     int top;
 } Stack;
 
-// 初始化栈
+// 初始化数值栈
+void initNumStack(NumStack* s) {
+    s->top = -1;
+}
+
+// 初始化运算符栈
 void initStack(Stack* s) {
     s->top = -1;
 }
 
-// 判断栈是否为空
+// 判断数值栈是否为空
+int isNumStackEmpty(NumStack* s) {
+    return s->top == -1;
+}
+
+// 判断运算符栈是否为空
 int isEmpty(Stack* s) {
     return s->top == -1;
 }
 
-// 判断栈是否已满
+// 判断运算符栈是否已满
 int isFull(Stack* s) {
     return s->top == MAX_SIZE - 1;
 }
 
-// 入栈
+// 数值栈入栈
+void pushNum(NumStack* s, double num) {
+    if (s->top < MAX_SIZE - 1) {
+        s->data[++(s->top)] = num;
+    }
+}
+
+// 运算符栈入栈
 void push(Stack* s, char c) {
     if (!isFull(s)) {
         s->data[++(s->top)] = c;
     }
 }
 
-// 出栈
+// 数值栈出栈
+double popNum(NumStack* s) {
+    if (!isNumStackEmpty(s)) {
+        return s->data[(s->top)--];
+    }
+    return 0;
+}
+
+// 运算符栈出栈
 char pop(Stack* s) {
     if (!isEmpty(s)) {
         return s->data[(s->top)--];
@@ -41,7 +72,7 @@ char pop(Stack* s) {
     return '\0';
 }
 
-// 获取栈顶元素
+// 获取运算符栈顶元素
 char peek(Stack* s) {
     if (!isEmpty(s)) {
         return s->data[s->top];
@@ -68,6 +99,48 @@ int getPriority(char c) {
         default:
             return 0;
     }
+}
+
+// 执行运算
+double calculate(double a, double b, char op) {
+    switch (op) {
+        case '+': return a + b;
+        case '-': return a - b;
+        case '*': return a * b;
+        case '/': return b != 0 ? a / b : 0;
+        case '^': {
+            double result = 1;
+            for (int i = 0; i < b; i++) {
+                result *= a;
+            }
+            return result;
+        }
+        default: return 0;
+    }
+}
+
+// 计算后缀表达式的值
+double evaluatePostfix(char* postfix) {
+    NumStack s;
+    initNumStack(&s);
+    int i = 0;
+    char c;
+
+    while ((c = postfix[i]) != '\0') {
+        if (isdigit(c)) {
+            // 如果是数字，直接入栈
+            pushNum(&s, c - '0');
+        } else if (isOperator(c)) {
+            // 如果是运算符，弹出两个操作数进行计算
+            double b = popNum(&s);
+            double a = popNum(&s);
+            double result = calculate(a, b, c);
+            pushNum(&s, result);
+        }
+        i++;
+    }
+
+    return popNum(&s);
 }
 
 // 中缀表达式转后缀表达式
@@ -117,11 +190,14 @@ int main() {
     char infix[MAX_SIZE];
     char postfix[MAX_SIZE];
 
-    printf("请输入中缀表达式: ");
+    printf("请输入中缀表达式（仅支持单个数字和四则运算）: ");
     scanf("%s", infix);
 
     infixToPostfix(infix, postfix);
     printf("后缀表达式: %s\n", postfix);
+    
+    double result = evaluatePostfix(postfix);
+    printf("计算结果: %.2f\n", result);
 
     return 0;
 }
